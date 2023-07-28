63c2c47f36b3548777d0fd21cfb5f62828a0289f
Yevgeniy <yev.bor@yandex.ru>
2023-07-28 17:10:14 +0300

    avators done

--- src/components/comments/SliderTest.tsx
+++ src/components/comments/SliderTest.tsx
@@ -28,16 +28,16 @@ export default function SliderTest() {
             <p>{clients[currentClient()].comment}</p>
           </div>
         </div>
-        <div class="w-3/12 grid grid-cols-5 grid-rows-4 gap-2">
+        <div class="w-3/12 grid grid-cols-5 grid-rows-auto gap-2">
           <For each={clients}>
             {(client, index) => (
               <div 
-                class={`image-${client.id}`}
+                class={`image__container-${client.id}`}
                 onClick={[handleClick, index]} 
               >
                 <img
                   alt="image"
-                  class="rounded-full   cursor-pointer object-cover object-top"
+                  class={`image-${client.id} rounded-full   cursor-pointer object-cover object-top`}
                   src={client.imageSrc}
                 />
               </div>
