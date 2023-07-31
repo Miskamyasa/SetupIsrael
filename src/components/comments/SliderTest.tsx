184b658de4e3c8da36a7f02c5846230f70a23b48
Yevgeniy <yev.bor@yandex.ru>
2023-07-31 15:26:14 +0300

    responsive

--- src/components/comments/SliderTest.tsx
+++ src/components/comments/SliderTest.tsx
@@ -11,40 +11,38 @@ export default function SliderTest() {
   };
 
   return (
-    <div>
-      <div class="flex slider mt-7">
-        <div class="flex w-9/12 ">
-          <div class="w-3/6">
-            <img
-              alt="image"
-              class=" rounded-full rounded-tr-none w-96 h-96  object-cover object-top"
-              src={clients[currentClient()].imageSrc}
-            />
-          </div>
-          <div class="w-3/6">
-            <h3>{`${clients[currentClient()].author.name} ${
-              clients[currentClient()].author.lastName
-            }`}</h3>
-            <p>{clients[currentClient()].comment}</p>
-          </div>
+    <div class="flex slider mt-7 flex-col md:flex-row">
+      <div class="flex md:w-9/12 flex-col md:flex-row items-center md:items-start">
+        <div class="md:w-3/6 order-last md:order-first">
+          <img
+            alt="image"
+            class=" rounded-full rounded-tr-none w-96 h-96  object-cover object-top"
+            src={clients[currentClient()].imageSrc}
+          />
         </div>
-        <div class="w-3/12 grid grid-cols-5 grid-rows-auto gap-2">
-          <For each={clients}>
-            {(client, index) => (
-              <div 
-                class={`image__container-${client.id}`}
-                onClick={[handleClick, index]} 
-              >
-                <img
-                  alt="image"
-                  class={`image-${client.id} rounded-full hover:scale-125 transition
-                   ease-in-out duration-150 cursor-pointer object-cover object-top`}
-                  src={client.imageSrc}
-                />
-              </div>
-            )}
-          </For>
+        <div class="md:w-3/6">
+          <h3>{`${clients[currentClient()].author.name} ${
+            clients[currentClient()].author.lastName
+          }`}</h3>
+          <p>{clients[currentClient()].comment}</p>
         </div>
+      </div>
+      <div class="md:w-3/12 md:grid md:grid-cols-5 md:grid-rows-auto md:gap-2 flex justify-center">
+        <For each={clients}>
+          {(client, index) => (
+            <div
+              class={`image__container-${client.id} `}
+              onClick={[handleClick, index]}
+            >
+              <img
+                alt="image"
+                class={`image-${client.id} rounded-full hover:scale-125 transition
+                   ease-in-out duration-150 cursor-pointer object-cover object-top avatar__size`}
+                src={client.imageSrc}
+              />
+            </div>
+          )}
+        </For>
       </div>
     </div>
   );
