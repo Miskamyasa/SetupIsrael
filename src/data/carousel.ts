644af3c3072de581c959e735a54c50fb60d2a282
Yevgeniy <yevgeniy@sunfltd.com>
2023-09-23 14:21:04 +0300

    modify slider

--- src/data/carousel.ts
+++ src/data/carousel.ts
@@ -4,9 +4,6 @@ import image55 from "../assets/gallery/55.jpg";
 import image56 from "../assets/gallery/56.jpg";
 import image57 from "../assets/gallery/57.jpg";
 import image58 from "../assets/gallery/58.jpg";
-// import { useTranslation } from "../i18n/translator"
-
-// const { t } = useTranslation()
 
 export interface CarouselImage {
   src: string
@@ -17,35 +14,34 @@ export interface CarouselImage {
 const carousel: CarouselImage[] = [
   {
     src: image53,
-    // title: t("gallery.53.title"),
-    // alt: t("gallery.53.alt"),
+    title: "Mock Title for Image 53",
+    alt: "Mock Alt for Image 53",
   },
   {
     src: image58,
-    // title: t("gallery.54.title"),
-    // alt: t("gallery.54.alt"),
+    title: "Mock Title for Image 58",
+    alt: "Mock Alt for Image 58",
   },
   {
     src: image55,
-    // title: t("gallery.55.title"),
-    // alt: t("gallery.55.alt"),
+    title: "Mock Title for Image 55",
+    alt: "Mock Alt for Image 55",
   },
   {
     src: image57,
-    // title: t("gallery.55.title"),
-    // alt: t("gallery.55.alt"),
+    title: "Mock Title for Image 57",
+    alt: "Mock Alt for Image 57",
   },
   {
     src: image56,
-    // title: t("gallery.55.title"),
-    // alt: t("gallery.55.alt"),
+    title: "Mock Title for Image 56",
+    alt: "Mock Alt for Image 56",
   },
   {
     src: image54,
-    // title: t("gallery.55.title"),
-    // alt: t("gallery.55.alt"),
+    title: "Mock Title for Image 54",
+    alt: "Mock Alt for Image 54",
   },
-
 ];
 
 export default carousel;
